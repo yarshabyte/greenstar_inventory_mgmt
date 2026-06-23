e8372b004d2a2855ffe0b64d264852e884243680
amrit <amrit44404@proton.me>
2026-06-23 21:37:09 +0545

    [Fix] core/urls.py: typo error fixed in `models.Category` and removed `Supplier` model.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,19 +1,9 @@
 from django.db import models
 from django.utils import choices, timezone
 
-class Category(model.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
-
-class Supplier(models.Model):
-    name = models.CharField(max_length=200)
-    contact_person = models.CharField(max_length=100, blank=True)
-    email = models.EmailField(blank=True)
-    phone = models.CharField(max_length=10, blank=True)
-    address = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -21,7 +11,6 @@ class Supplier(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
-    suppliers = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True, related_name='products')
     description = models.TextField(blank=True)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     quantity = models.PositiveIntegerField(default=0)
